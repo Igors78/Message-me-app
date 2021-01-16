13fdad140b48e7bb941053294bbdf14d9463af5a
Igors78 <oleinikovsigors@gmail.com>
2021-01-16 21:02:03 +0200

    Add styling to signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_back_or user
