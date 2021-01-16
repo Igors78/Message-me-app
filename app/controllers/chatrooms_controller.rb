5f8626622a7bd555bcaa8505474431350d0a0747
Igors78 <oleinikovsigors@gmail.com>
2021-01-16 21:54:29 +0200

    Add message resource

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ChatroomsController < ApplicationController
+  def index
+    @messages = Message.all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true
+  has_many :messages
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 },
+                   uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
 end
