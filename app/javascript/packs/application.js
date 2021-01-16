0091f7d4b178a7fd677c4008388ed029103b9ec1
Igors78 <oleinikovsigors@gmail.com>
2021-01-16 16:14:18 +0200

    Connect bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,22 @@ ActiveStorage.start();
 // Loads all Semantic javascripts
 //= require jquery
 //= require semantic-ui
+
+import * as bootstrap from "bootstrap";
+import "../stylesheets/application";
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
