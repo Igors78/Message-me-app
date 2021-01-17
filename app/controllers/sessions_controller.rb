373a201d72493adb67ccb04abdbb1603c6a5cea2
Igors78 <oleinikovsigors@gmail.com>
2021-01-17 15:21:40 +0200

    Add message partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new; end
 
   def create
     user = User.find_by(name: params[:session][:name])
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
-      redirect_back_or user
+      flash[:success] = 'Successfully logged in!'
+      redirect_to root_path
     else
       flash.now[:danger] = 'Invalid name/password combination'
       render 'new'
@@ -14,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root_url
+    redirect_to login_path
   end
 end
