060f98c477ceb949842435850d7ac7ca6e486837
Igors78 <oleinikovsigors@gmail.com>
2021-01-19 09:09:53 +0200

    Deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_cable.allowed_request_origins = ['https://message-me-action-cable.herokuapp.com',
+                                                 'http://message-me-action-cable.herokuapp.com']
+  config.web_socket_server_url = 'wss://action-cable-example.herokuapp.com/cable'
 end
